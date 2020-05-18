97aebac5934a5dc33954e0cea3eb6286a7edbb1f
Oleg Gritchin <oleg.gr@outlook.com>
2020-05-18 18:48:25 +0300

    some server changes

--- s.c
+++ s.c
@@ -37,6 +37,7 @@ int main(int argc, char *argv[])
     char client_message[200]= {0};
     //char message[100] = {0};
     // char *message = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\nConnection: close\r\n\r\nHello, world!";
+    char *message;
     char *auth_token = "token_value";
 
     const char *pMessage = "Authenfication:token_value";
@@ -109,11 +110,11 @@ int main(int argc, char *argv[])
 
         if(strcmp(pMessage,client_message)==0)
         {
-            char *message = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\nConnection: close\r\n\r\nHello, world!";
+             message = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\nConnection: close\r\n\r\nHello, world!";
         }
         else
         {
-            char *message = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\nConnection: close\r\n\r\nWrong token!";
+            message = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\nConnection: close\r\n\r\nWrong token!";
         }
 
 
